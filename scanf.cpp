9ad01b5374541518dc6497ab2dddbd4b110b253f
HelkerVonMises <94910396+HelkerVonMises@users.noreply.github.com>
2021-11-26 16:33:19 -0300

    Add files via upload

---
+++ scanf.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+main ()
+{
+int a,b,c;
+printf("Digite um número\n");
+scanf("%i", &a);
+printf("digite outro valor\n");
+scanf("%i, &b");
+c=a+b;
+printf("\n Resultado = %i\n", c);
+}
