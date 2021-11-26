2b5aadc064173cda85373d2c840fe835c66402d5
HelkerVonMises <94910396+HelkerVonMises@users.noreply.github.com>
2021-11-26 17:37:29 -0300

    Add files via upload

---
+++ 4 - Raiz quinta.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+#include <conio.h>
+
+int main(){
+   float a, b;
+   printf ("Digite o número para efetuar a raiz quinta: ");
+   scanf("%f", &a);
+   b=pow(a,0.2);
+   printf("%f",b);
+}
