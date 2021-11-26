7d9f08eecef0a52e713160d4f8aab3916025cb1d
HelkerVonMises <94910396+HelkerVonMises@users.noreply.github.com>
2021-11-26 17:01:17 -0300

    Update Soma.c

--- Soma.c
+++ Soma.c
@@ -3,22 +3,23 @@
 
 int main(){
     int a, b, c, d;
+    
     inicio:
     printf("Programa de soma\n");
     printf("Digite o primeiro número\n");
-    scanf("%i", &a);
+        scanf("%i", &a);
     printf ("digite outro número\n");
-    scanf("%i", &b);
+        scanf("%i", &b);
 c=a+b;
     printf("Resultado de %i+%i é %i\n", a, b,c);
     printf("Deseja realizar outra soma?\n");
     printf("1 para sim e 2 para não\n");
-    scanf("%i", &d);
+        scanf("%i", &d);
     if(d==1){
         goto inicio;
     }
     if(d==2){
-      printf("É nóis parça");
+        printf("É nóis parça");
     }
       else{
           printf("Tá de sacanagem???\n");
